6dfc81ea342c6335e31ded518aef7b0447296b3d
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-01-18 06:54:35 +0000

    Release 0.0.590

--- src/revert/core/client_wrapper.py
+++ src/revert/core/client_wrapper.py
@@ -13,7 +13,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "revert-api",
-            "X-Fern-SDK-Version": "0.0.588",
+            "X-Fern-SDK-Version": "0.0.590",
         }
         return headers
 
